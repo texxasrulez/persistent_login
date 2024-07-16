b6aa526f4b63291cb6bc744818a5f8bc4f0c40b2
Jan Pechek <honza@pechek.cz>
2024-07-16 16:30:52 +0200

    fix: sql syntax for mariadb

--- SQL/mariadb.initial.sql
+++ SQL/mariadb.initial.sql
@@ -7,18 +7,18 @@
  */
 
 CREATE TABLE IF NOT EXISTS auth_tokens (
-    `token` varchar(128) NOT NULL,
+    `token` varchar(128) NOT NULL PRIMARY KEY,
     `expires` datetime NOT NULL,
-    `user_id` int check (`user_id` > 0) NOT NULL,
+    `user_id` int unsigned NOT NULL CHECK(`user_id` > 0),
     `user_name` varchar(128) NOT NULL,
     `user_pass` varchar(128) NOT NULL,
-	`host` varchar(255) NOT NULL
-   ,
-	PRIMARY KEY (`token`),
-	CONSTRAINT `auth_tokens_ibfk_1` FOREIGN KEY (`user_id`)
-	REFERENCES users(`user_id`) ON DELETE CASCADE
-) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+    `host` varchar(255) NOT NULL,
+    CONSTRAINT `auth_tokens_ibfk_1`
+        FOREIGN KEY (`user_id`)
+        REFERENCES users(`user_id`)
+        ON DELETE CASCADE
+);
 
 CREATE INDEX `user_id_fk_auth_tokens` ON auth_tokens (`user_id`);
 
-REPLACE INTO `system` (`name`, `value`) SELECT ('tx-persistent-login-version', '2020080200');
+REPLACE INTO `system` (`name`, `value`) VALUES ('tx-persistent-login-version', '2020080200');
