bc2cfa78be662f41d10b5c7d1c447e5b6ec694c0
Gene Hawkins <texxasrulez@yahoo.com>
2026-05-26 00:51:42 -0500

    Update mariadb.initial.sql
    Fix for issue #1

--- SQL/mariadb.initial.sql
+++ SQL/mariadb.initial.sql
@@ -9,7 +9,7 @@
 CREATE TABLE IF NOT EXISTS auth_tokens (
     `token` varchar(128) NOT NULL,
     `expires` datetime NOT NULL,
-    `user_id` int check (`user_id` > 0) NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
     `user_name` varchar(128) NOT NULL,
     `user_pass` varchar(128) NOT NULL,
 	`host` varchar(255) NOT NULL
